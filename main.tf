980fb06632fb6b841a0222fb32d1d04ba9ca87d5
Vicente <vvenancio06@gmail.com>
2026-03-09 10:40:04 -0300

    feat: call modules on main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
 module "dependencies" {
   source = "./modules/dependencies"
 }
+
+module "gpu" {
+  source = "./modules/gpu"
+
+  depends_on = [modules.dependencies]
+}
+
+module "cluster" {
+  source = "./modules/cluster"
+
+  depends_on = [modules.gpu]
+
+    cluster_name = var.kubeflow
+}
